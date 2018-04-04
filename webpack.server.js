0b931614f9be03306eb3caf649bd208fbaf264f8
Raul Gutierrez <raul.gutierrez@verys.com>
2018-04-03 17:48:21 -0700

    building client and server

---
+++ webpack.server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+
+const config = {
+  // Inform webpack that we're building a bundle for nodeJS, rather than for the browser
+  target: 'node',
+
+  // Tell webpack the root file of our server application
+  entry: './src/index.js',
+
+  // Tell webpack where to put the output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+};
+
+module.exports = merge(baseConfig, config);
