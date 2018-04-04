4c97754c79ce9624a53084d908552cf39163fdda
Raul Gutierrez <raul.gutierrez@verys.com>
2018-04-03 17:59:22 -0700

    added renderer helper, included webpack-node-externals

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that we're building a bundle for nodeJS, rather than for the browser
@@ -13,7 +14,9 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
