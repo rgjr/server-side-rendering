3382effb607c312a9b8812d1436ed1537a584ad2
Raul Gutierrez <raul.gutierrez@verys.com>
2018-04-06 13:03:54 -0700

    added matchRoutes() and created loadData() to send data through to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -9,6 +11,10 @@ app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
+
+  matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData() : null;
+  });
 
   res.send(renderer(req, store));
 });
