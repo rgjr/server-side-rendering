74f1aeab45eb3dbf3ab4d3b3a38f02bf42a07066
Raul Gutierrez <raul.gutierrez@verys.com>
2018-04-11 12:12:40 -0700

    added routing and links for authentication

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -5,9 +5,20 @@ import { connect } from 'react-redux';
 const Header = ({ auth }) => {
   console.log('My auth status is: ', auth);
 
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
   return (
     <div>
       <Link to="/">React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admin">Admins</Link>
+        {authButton}
+      </div>
     </div>
   );
 };
