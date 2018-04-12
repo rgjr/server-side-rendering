4cc7a4a1550a929f24b97c4c111e4192f85f8f23
Raul Gutierrez <raul.gutierrez@verys.com>
2018-04-12 12:59:16 -0700

    included react-helmet for seo

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,8 +3,9 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
-import Routes from '../client/Routes';
 import serialize from 'serialize-javascript';
+import { Helmet } from 'react-helmet';
+import Routes from '../client/Routes';
 
 export default (req, store, context) => {
   const content = renderToString(
@@ -15,10 +16,13 @@ export default (req, store, context) => {
     </Provider>
   );
 
+  const helmet = Helmet.renderStatic();
+
   return `
     <html>
       <head>
-        <!-- Compiled and minified CSS -->
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
       </head>
       <body>
